1dcb0d1cabc2661e563dd489e92d7877bcafb196
Kenan Soylu <keso@itu.dk>
2017-12-01 17:21:39 +0100

    ViewModels

---
+++ CrowdSpark.App/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CrowdSpark.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrowdSpark.App.ViewModels
+{
+    class MainPageViewModel : BaseViewModel
+    {
+        public ObservableCollection<ProjectPageViewModel> Projects { get; private set; }
+        public ObservableCollection<string> ProjectNames { get; set; }
+        private IProjectsRepository _repository;
+
+        public MainPageViewModel(ICrowdSparkContext repository)
+        {
+            _repository = repository;
+            Projects = repository.
+        }
+
+        public 
+    }
+}
