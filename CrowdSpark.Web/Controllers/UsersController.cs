7a1e05d31a4ab4bf4653c70bb4206e59248f9b47
Michael Michalski <michaelmartinmichalski@gmail.com>
2017-12-01 17:01:22 +0100

    setting up routing

---
+++ CrowdSpark.Web/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrowdSpark.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController
+    {
+        // GET api/users/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "specific user";
+        }
+
+        // GET api/users/skills
+        [HttpGet("skills")]
+        public IEnumerable<string> GetSkills()
+        {
+            return new string[] { "programming", "pyrotechnics" };
+        }
+
+        [HttpPost("skills/{name}")]
+        public string GetSkill([FromBody]string name)
+        {
+//            return "ok, skill already present!";
+            return "skill "+ name + " added!";
+        }
+
+        /*   [Route("api/users/[controller]")]
+           public class SkillsController
+           {
+
+           }*/
+    }
+
+}
