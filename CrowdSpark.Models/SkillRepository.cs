73f879299d65085e1b3216bb2034e1cdd8a3c4f3
Sam Aaron Parkinson <sapa@itu.dk>
2017-12-04 05:04:09 +1100

    Added Skill repo, associated Interface, and injection

---
+++ CrowdSpark.Models/SkillRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using CrowdSpark.Entitites;
+using CrowdSpark.Common;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrowdSpark.Models
+{
+    public class SkillRepository : ISkillRepository
+    {
+        private readonly ICrowdSparkContext _context;
+
+        public SkillRepository(ICrowdSparkContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> CreateAsync(Skill skill)
+        {
+            var skillToCreate = new Skill
+            {
+                Name = skill.Name
+            };
+
+            _context.Skills.Add(skillToCreate);
+            if (await _context.SaveChangesAsync() > 0)
+            {
+                return skillToCreate.Id;
+            }
+            else throw new DbUpdateException("Error creating skill", (Exception)null);
+        }
+
+        public async Task<bool> DeleteAsync(int skillId)
+        {
+            var skill = await _context.Skills.FindAsync(skillId);
+            _context.Skills.Remove(skill);
+
+            return ( await _context.SaveChangesAsync() > 0 );
+        }
+
+        public async Task<Skill> FindAsync(int skillId)
+        {
+            return await _context.Skills.FindAsync(skillId);
+        }
+
+        public async Task<IReadOnlyCollection<Skill>> ReadAsync()
+        {
+            return await _context.Skills.ToListAsync();
+        }
+
+        public async Task<bool> UpdateAsync(Skill details)
+        {
+            var skillToUpdate = await _context.Skills.FindAsync(details.Id);
+            _context.Skills.Update(skillToUpdate);
+
+            skillToUpdate.Name = details.Name;
+
+            return (await _context.SaveChangesAsync() > 0);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
--- CrowdSpark.Web/Startup.cs
+++ CrowdSpark.Web/Startup.cs
@@ -38,6 +38,7 @@ namespace CrowdSpark
             services.AddScoped<IProjectRepository, ProjectRepository>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ISkillRepository, SkillRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
