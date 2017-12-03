6e85cd53c6347886c434e34f31345ce900623577
Sam Aaron Parkinson <sapa@itu.dk>
2017-12-04 04:49:05 +1100

    Added Category Repo, associated DTO, and interface

---
+++ CrowdSpark.Models/CategoryRepository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using CrowdSpark.Entitites;
+using CrowdSpark.Common;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrowdSpark.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ICrowdSparkContext _context;
+
+        public CategoryRepository(ICrowdSparkContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> CreateAsync(CategoryCreateDTO category)
+        {
+            var categoryToCreate = new Category
+            {
+                Name = category.Name
+            };
+
+            _context.Categorys.Add(categoryToCreate);
+            if (await _context.SaveChangesAsync() > 0)
+            {
+                return categoryToCreate.Id;
+            }
+            else throw new DbUpdateException("Error creating category", (Exception)null);
+        }
+
+        public async Task<bool> DeleteAsync(int categoryId)
+        {
+            var category = await _context.Categorys.FindAsync(categoryId);
+            _context.Categorys.Remove(category);
+
+            return ( await _context.SaveChangesAsync() > 0 );
+        }
+
+        public async Task<Category> FindAsync(int categoryId)
+        {
+            var category =  await _context.Categorys.FindAsync(categoryId);
+
+            return new Category
+            {
+                Id = category.Id,
+                Name = category.Name
+            };
+        }
+
+        public async Task<IReadOnlyCollection<Category>> ReadAsync()
+        {
+            var categories = from c in _context.Categorys
+                           select new Category
+                           {
+                               Id = c.Id,
+                               Name = c.Name
+                           };
+
+            return await categories.ToListAsync();
+        }
+
+        public async Task<bool> UpdateAsync(Category details)
+        {
+            var categoryToUpdate = await _context.Categorys.FindAsync(details.Id);
+            _context.Categorys.Update(categoryToUpdate);
+
+            categoryToUpdate.Name = details.Name;
+
+            return (await _context.SaveChangesAsync() > 0);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
