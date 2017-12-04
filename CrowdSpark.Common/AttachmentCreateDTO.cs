4b88d079834abf1f58067bfd8ba2affe1af81da9
Sam Aaron Parkinson <sapa@itu.dk>
2017-12-05 00:39:26 +1100

    Added Attachment Repo

---
+++ CrowdSpark.Common/AttachmentCreateDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CrowdSpark.Common
+{
+    public class AttachmentCreateDTO
+    {
+        public string Description { get; set; }
+
+        [Required]
+        public string Data { get; set; }
+
+        [Required]
+        public int Type { get; set; }
+    }
+}
---
+++ CrowdSpark.Common/IAttachmentRepository.cs
@@ -0,0 +1,21 @@
+﻿using CrowdSpark.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrowdSpark.Common
+{
+    public interface IAttachmentRepository : IDisposable
+    {
+        Task<int> CreateAsync(AttachmentCreateDTO attatchment);
+     
+        Task<Attachment> FindAsync(int attachmentId);
+
+        Task<IReadOnlyCollection<Attachment>> ReadAsync();
+
+        Task<bool> UpdateAsync(Attachment details);
+
+        Task<bool> DeleteAsync(int attachmentId);
+    }
+}
