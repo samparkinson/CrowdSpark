8c52359f331cddef8baed9f6a248adf98aef18a7
Michael Michalski <michaelmartinmichalski@gmail.com>
2017-12-05 17:08:10 +0100

    authentication webapp

--- CrowdSpark.Web/Startup.cs
+++ CrowdSpark.Web/Startup.cs
@@ -12,6 +12,15 @@ using Microsoft.Extensions.Options;
 using CrowdSpark.Entitites;
 using CrowdSpark.Common;
 using CrowdSpark.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CrowdSpark
 {
@@ -27,6 +36,23 @@ namespace CrowdSpark
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting(o =>
+            {
+                o.LowercaseUrls = true;
+            });
+
+            var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                //.RequireRole("Admin", "SuperUser")
+                .Build();
+            /*
+            services.Configure<MvcOptions>(o =>
+            {
+                o.Filters.Add(new RequireHttpsAttribute());
+                o.Filters.Add(new AuthorizeFilter(policy));
+            });
+            */
+
             services.AddMvc();
 
             services.AddDbContext<CrowdSparkContext>( o =>
@@ -41,6 +67,29 @@ namespace CrowdSpark
             services.AddScoped<ILocationRepository, LocationRepository>();
             services.AddScoped<IAttachmentRepository, AttachmentRepository>();
             services.AddScoped<ISparkRepository, SparkReposiory>();
+
+            var options = new AzureAdOptions();
+            Configuration.Bind("AzureAd", options);
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(o =>
+            {
+                //o.Audience = options.Audience;
+                o.Authority = options.Authority;
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    ValidAudiences = new[] { options.ClientId, options.Audience }
+                };
+            });
+
+            services.AddMvc(config =>
+            {
+                config.RespectBrowserAcceptHeader = true;
+                config.InputFormatters.Add(new XmlSerializerInputFormatter());
+                config.OutputFormatters.Add(new XmlSerializerOutputFormatter());
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +104,17 @@ namespace CrowdSpark
             {
                 app.UseExceptionHandler("/Web/Error");
             }
+
+        //    var options = new RewriteOptions().AddRedirectToHttps();
+        //    app.UseRewriter(options);
+
+            app.UseAuthentication();
+
+            // Enable CORS - you probably want to limit this to known origins.
+            app.UseCors(builder =>
+                 builder.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
 
             app.UseStaticFiles();
 
