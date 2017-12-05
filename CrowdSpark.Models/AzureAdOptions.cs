6694b5ccc3cd3efda1c7f8b2ecae45e81419e76d
Michael Michalski <michaelmartinmichalski@gmail.com>
2017-12-05 17:08:10 +0100

    authentication webapp

---
+++ CrowdSpark.Models/AzureAdOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrowdSpark.Models
+{
+    public class AzureAdOptions
+    {
+        public string Instance { get; set; }
+
+        public string Domain { get; set; }
+
+        public string TenantId { get; set; }
+
+        public string ClientId { get; set; }
+
+        public string Audience { get; set; }
+
+        public string Authority => $"{Instance}{TenantId}";
+    }
+}
