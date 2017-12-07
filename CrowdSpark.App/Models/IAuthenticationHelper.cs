1ea6ffcccb55b6e2f92a5fe8b96ae5bc8b531e33
Michael Michalski <michaelmartinmichalski@gmail.com>
2017-12-07 13:18:20 +0100

    settings for authentication

--- CrowdSpark.App/Models/IAuthenticationHelper.cs
+++ CrowdSpark.App/Models/IAuthenticationHelper.cs
@@ -1,6 +1,13 @@
-﻿namespace CrowdSpark.App.Models
+﻿using System.Threading.Tasks;
+using Windows.Security.Credentials;
+
+namespace CrowdSpark.App.Models
 {
     public interface IAuthenticationHelper
     {
+        Task<WebAccount> SignInAsync();
+        Task SignOutAsync(WebAccount account);
+        Task<string> AcquireTokenSilentAsync();
+        Task<WebAccount> GetAccountAsync();
     }
 }
