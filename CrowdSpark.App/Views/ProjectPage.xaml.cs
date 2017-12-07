1946e5509e2a7d386ddf50fe86b888e043d62b11
Kenan Soylu <keso@itu.dk>
2017-12-07 12:46:21 +0100

    Fixes in UI, new CurrentUser class

--- CrowdSpark.App/Views/ProjectPage.xaml.cs
+++ CrowdSpark.App/Views/ProjectPage.xaml.cs
@@ -1,20 +1,9 @@
 ﻿using CrowdSpark.App.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
