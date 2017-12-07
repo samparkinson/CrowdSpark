e4ae9399a08b0451e3c6db4597255ccabfbbc8fe
Kenan Soylu <keso@itu.dk>
2017-12-07 11:32:09 +0100

    New menu design

---
+++ CrowdSpark.App/Helpers/HamburgerMenuOptionsFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace CrowdSpark.App.Helpers
+{
+    class HamburgerMenuOptionsFactory
+    {
+        public ObservableCollection<MenuOption> MenuOptions { get; set; }
+
+        public HamburgerMenuOptionsFactory(string UserName)
+        {
+            MenuOptions = new ObservableCollection<MenuOption>();
+            
+            MenuOptions.Add(new MenuOption("Account", UserName));
+            MenuOptions.Add(new MenuOption("Page", "Projects"));
+            MenuOptions.Add(new MenuOption("Setting", "Settings"));
+            MenuOptions.Add(new MenuOption("Message", "Feedback"));
+        }
+        
+    }
+}
---
+++ CrowdSpark.App/Helpers/MenuOption.cs
@@ -0,0 +1,16 @@
+﻿namespace CrowdSpark.App.Helpers
+{
+    class MenuOption
+    {
+        public string Text { get; set; }
+
+        public string Icon { get; set; }
+
+        public MenuOption(string Icon, string Text)
+        {
+            this.Text = Text;
+
+            this.Icon = Icon;
+        }
+    }
+}
