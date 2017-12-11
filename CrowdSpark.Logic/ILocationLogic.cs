1e3e97f9c0c9b44a166886465ab710d1b1861563
Sam Aaron Parkinson <sapa@itu.dk>
2017-12-11 21:52:22 +1100

    Progress on location logic

---
+++ CrowdSpark.Logic/ILocationLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CrowdSpark.Common;
+using CrowdSpark.Entitites;
+
+namespace CrowdSpark.Logic
+{
+    public interface ILocationLogic
+    {
+        Task<IEnumerable<Location>> GetAsync();
+
+        Task<Location> GetAsync(int locationId);
+
+        Task<IEnumerable<Location>> FindAsync(string city, string country);
+
+        Task<IEnumerable<Location>> FindAsync(string city);
+
+        Task<Location> FindExactAsync(string city, string country);
+
+        Task<ResponseLogic> CreateAsync(Location skill);
+
+        Task<ResponseLogic> UpdateAsync(Location skill);
+
+        Task<ResponseLogic> DeleteAsync(int locationId);
+    }
+}
---
+++ CrowdSpark.Logic/LocationLogic.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CrowdSpark.Common;
+using CrowdSpark.Entitites;
+
+namespace CrowdSpark.Logic
+{
+    public class LocationLogic : ILocationLogic
+    {
+        public async Task<ResponseLogic> CreateAsync(Location skill)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ResponseLogic> DeleteAsync(int locationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Location>> FindAsync(string city, string country)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Location>> FindAsync(string city)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Location> FindExactAsync(string city, string country)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Location>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Location> GetAsync(int locationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ResponseLogic> UpdateAsync(Location skill)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
