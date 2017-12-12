7d075f23305fbc843aa799bbb73b3e29fd38fcd2
Sam Aaron Parkinson <sapa@itu.dk>
2017-12-13 09:38:31 +1100

    Added CrowdSpark.Logic.Tests project and test classes

---
+++ CrowdSpark.Logic.Tests/SparkLogicTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CrowdSpark.Logic.Tests
+{
+    public class SparkLogicTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ CrowdSpark.Logic.Tests/UserLogicTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CrowdSpark.Logic.Tests
+{
+    public class UserLogicTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
