b646d74c5730e440953277c6c0b3a12918687042
Sam Aaron Parkinson <sapa@itu.dk>
2017-12-13 23:22:21 +1100

    CategoryLogicTests Progress

--- CrowdSpark.Logic.Tests/CategoryLogicTests.cs
+++ CrowdSpark.Logic.Tests/CategoryLogicTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using CrowdSpark.Common;
 using CrowdSpark.Entitites;
 using CrowdSpark.Logic;
@@ -12,12 +13,12 @@ namespace CrowdSpark.Logic.Tests
     {
         private readonly Mock<ICategoryRepository> categoryRepositoryMock;
         private readonly CategoryRepository categoryRepository;
-        private readonly Mock<IProjectRepository> _projectRepository;
+        private readonly Mock<IProjectRepository> projectRepositoryMock;
 
         public CategoryLogicTests()
         {
             categoryRepositoryMock = new Mock<ICategoryRepository>();
-            _projectRepository = new Mock<IProjectRepository>();
+            projectRepositoryMock = new Mock<IProjectRepository>();
         }
 
         public void Dispose()
@@ -43,7 +44,7 @@ namespace CrowdSpark.Logic.Tests
             categoryRepositoryMock.Setup(c => c.FindAsync(categoryToCreate.Name)).ReturnsAsync(default(Category));
             categoryRepositoryMock.Setup(c => c.CreateAsync(categoryToCreate)).ReturnsAsync(1);
 
-            using (var logic = new CategoryLogic(categoryRepositoryMock.Object, _projectRepository.Object))
+            using (var logic = new CategoryLogic(categoryRepositoryMock.Object, projectRepositoryMock.Object))
             {
                 var response = await logic.CreateAsync(categoryToCreate);
 
@@ -69,13 +70,108 @@ namespace CrowdSpark.Logic.Tests
 
             categoryRepositoryMock.Setup(c => c.FindAsync(categoryToCreate.Name)).ReturnsAsync(existingCategory);
 
-            using (var logic = new CategoryLogic(categoryRepositoryMock.Object, _projectRepository.Object))
+            using (var logic = new CategoryLogic(categoryRepositoryMock.Object, projectRepositoryMock.Object))
             {
                 var response = await logic.CreateAsync(categoryToCreate);
 
                 Assert.Equal(ResponseLogic.SUCCESS, response);
                 categoryRepositoryMock.Verify(c => c.FindAsync(categoryToCreate.Name));
                 categoryRepositoryMock.Verify(c => c.CreateAsync(It.IsAny<CategoryCreateDTO>()), Times.Never());
+            }
+        }
+
+        [Fact]
+        public async void CreateAsync_GivenNothingCreated_ReturnsERROR_CREATING()
+        {
+            var categoryToCreate = new CategoryCreateDTO
+            {
+                Name = "New Category"
+            };
+
+            categoryRepositoryMock.Setup(c => c.FindAsync(categoryToCreate.Name)).ReturnsAsync(default(Category));
+            categoryRepositoryMock.Setup(c => c.CreateAsync(categoryToCreate)).ReturnsAsync(0);
+
+            using (var logic = new CategoryLogic(categoryRepositoryMock.Object, projectRepositoryMock.Object))
+            {
+                var response = await logic.CreateAsync(categoryToCreate);
+
+                Assert.Equal(ResponseLogic.ERROR_CREATING, response);
+                categoryRepositoryMock.Verify(c => c.FindAsync(categoryToCreate.Name));
+                categoryRepositoryMock.Verify(c => c.CreateAsync(categoryToCreate));
+            }
+        }
+
+        [Fact]
+        public async void FindAsync_GivenCategoriesExist_ReturnsEnumerableCategories()
+        {
+            var categoriesToReturn = new Category[]
+            {
+                new Category { Id = 1, Name = "This is the first" },
+                new Category { Id = 2, Name = "This is the second" },
+                new Category { Id = 3, Name = "This is the third" }
+            };
+
+            categoryRepositoryMock.Setup(c => c.FindWildcardAsync("this")).ReturnsAsync(categoriesToReturn);
+
+            using (var logic = new CategoryLogic(categoryRepositoryMock.Object, projectRepositoryMock.Object))
+            {
+                var results = await logic.FindAsync("this");
+
+                Assert.Equal(categoriesToReturn, results);
+                categoryRepositoryMock.Verify(c => c.FindWildcardAsync("this"));
+            }
+        }
+
+        [Fact]
+        public async void FindExactAsync_GivenCategoryExists_ReturnsCategory()
+        {
+            var categoryToReturn = new Category { Id = 1, Name = "This is the first" };
+
+            categoryRepositoryMock.Setup(c => c.FindAsync("This is the first")).ReturnsAsync(categoryToReturn);
+
+            using (var logic = new CategoryLogic(categoryRepositoryMock.Object, projectRepositoryMock.Object))
+            {
+                var result = await logic.FindExactAsync("This is the first");
+
+                Assert.Equal(categoryToReturn, result);
+                categoryRepositoryMock.Verify(c => c.FindAsync("This is the first"));
+            }
+        }
+
+        [Fact]
+        public async void GetAsync_GivenCategoriesExist_ReturnsEnumerableCategories()
+        {
+            var categoriesToReturn = new Category[]
+            {
+                new Category { Id = 1, Name = "This is the first" },
+                new Category { Id = 2, Name = "This is the second" },
+                new Category { Id = 3, Name = "This is the third" }
+            };
+
+            categoryRepositoryMock.Setup(c => c.ReadAsync()).ReturnsAsync(categoriesToReturn);
+
+            using (var logic = new CategoryLogic(categoryRepositoryMock.Object, projectRepositoryMock.Object))
+            {
+                var results = await logic.GetAsync();
+
+                Assert.Equal(categoriesToReturn, results);
+                categoryRepositoryMock.Verify(c => c.ReadAsync());
+            }
+        }
+
+        [Fact]
+        public async void GetAsync_GivenExistingCategoryId_ReturnsCategory()
+        {
+            var categoryToReturn = new Category { Id = 3, Name = "This is a Category" };
+
+            categoryRepositoryMock.Setup(c => c.FindAsync(3)).ReturnsAsync(categoryToReturn);
+
+            using (var logic = new CategoryLogic(categoryRepositoryMock.Object, projectRepositoryMock.Object))
+            {
+                var result = await logic.GetAsync(3);
+
+                Assert.Equal(categoryToReturn, result);
+                categoryRepositoryMock.Verify(c => c.FindAsync(3));
             }
         }
 
