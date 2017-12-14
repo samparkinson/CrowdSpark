9bb44a8c8e39d845bd1625fa157088bdaf5fc539
Robert Erdoedy <robert.erdoedy@yahoo.com>
2017-12-14 13:06:37 +0100

    added skeleton for SparkAPI

---
+++ CrowdSpark.App/Models/ISparkAPI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrowdSpark.Common;
+
+namespace CrowdSpark.App.Models
+{
+    public interface ISparkAPI : IDisposable
+    {
+        Task<IReadOnlyCollection<SparkDTO>> GetSparks();
+    }
+}
---
+++ CrowdSpark.App/Models/SparkAPI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using CrowdSpark.Common;
+
+namespace CrowdSpark.App.Models
+{
+    public class SparkAPI : ISparkAPI
+    {
+        private readonly HttpClient _client;
+
+        public SparkAPI(ISettings settings, DelegatingHandler handler)
+        {
+            var client = new HttpClient(handler)
+            {
+                BaseAddress = settings.ApiBaseAddress
+            };
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            _client = client;
+        }
+        public Task<IReadOnlyCollection<SparkDTO>> GetSparks()
+        {
+            throw new NotImplementedException();
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
