c2abc892537e42690d445dbf63eb7ef2b1c89bef
Michael Michalski <michaelmartinmichalski@gmail.com>
2017-12-16 12:45:51 +0100

    added some tests

--- CrowdSpark.Models.Tests/CategoryRepositoryTests.cs
+++ CrowdSpark.Models.Tests/CategoryRepositoryTests.cs
@@ -98,5 +98,82 @@ namespace CrowdSpark.Models.Tests
                 }
             }
         }
+
+        [Fact]
+        public async void FindWildcardAsync_EmptyCollection()
+        {
+            var list = new List<CategoryCreateDTO>();
+
+            list.Add(new CategoryCreateDTO
+            {
+                Name = "Programming"
+            });
+
+            list.Add(new CategoryCreateDTO
+            {
+                Name = "Mechanical"
+            });
+
+            list.Add(new CategoryCreateDTO
+            {
+                Name = "Sport"
+            });
+
+            list.Add(new CategoryCreateDTO
+            {
+                Name = "Outlife"
+            });
+
+            using (var repository = new CategoryRepository(context))
+            {
+                var id_list = new HashSet<int>();
+                for (int i = 0; i < list.Count; i++)
+                {
+                    id_list.Add(await repository.CreateAsync(list[i]));
+                }
+
+                Assert.Equal(id_list.Count, list.Count);
+
+                var sorted_list = await repository.FindWildcardAsync("Arduino");
+                var listToCheck = new List<CategoryDTO>(sorted_list);
+
+                Assert.Equal(0, listToCheck.Count);
+
+                sorted_list = await repository.FindWildcardAsync("D");
+                listToCheck = new List<CategoryDTO>(sorted_list);
+                Assert.True(listToCheck.Count > 0);
+            }
+        }
+
+        [Fact]
+        public async void DeleteAsync_FalseIfNotExists()
+        {
+            var cat = new CategoryCreateDTO
+            {
+                Name = "Programming"
+            };
+
+            using (var repository = new CategoryRepository(context))
+            {
+                var id = await repository.CreateAsync(cat);
+                Assert.False(await repository.DeleteAsync(id+1));
+            }
+        }
+
+        [Fact]
+        public async void DeleteAsync_TrueifExists_ActuallyDeletes()
+        {
+            var cat = new CategoryCreateDTO
+            {
+                Name = "Programming"
+            };
+
+            using (var repository = new CategoryRepository(context))
+            {
+                var id = await repository.CreateAsync(cat);
+                Assert.True(await repository.DeleteAsync(id));
+                Assert.Equal(0, await context.Categories.CountAsync());
+            }
+        }
     }
 }
