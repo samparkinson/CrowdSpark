9503f8e9a277c9bd53760ebd380037d45cb1dc4a
Sam Aaron Parkinson <sapa@itu.dk>
2017-12-17 21:54:30 +1100

    Added tests in skilllogic for search wildcard and search excat

--- CrowdSpark.Logic.Tests/SkillLogicTests.cs
+++ CrowdSpark.Logic.Tests/SkillLogicTests.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Linq;
 using CrowdSpark.Common;
 using CrowdSpark.Entitites;
 using CrowdSpark.Models;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Xunit;
 
@@ -105,6 +108,84 @@ namespace CrowdSpark.Logic.Tests
                 skillRepositoryMock.Verify(c => c.CreateAsync(skillToCreate));
             }
         }
+
         #endregion
+
+        #region IntegreationTests
+
+        [Fact]
+        public async void SearchAsync_GivenMatchingSkills_ReturnsSkills()
+        {
+            var existingSkills = new Skill[]
+            {
+                new Skill() { Id = 1, Name = "Cooking" },
+                new Skill() { Id = 2, Name = "Cooking Bacon"},
+                new Skill() { Id = 3, Name = "Burning things while trying to cook"},
+                new Skill() { Id = 4, Name = "Yoddeling"}
+            };
+
+            skillRepository = new SkillRepository(setupContextForIntegrationTests());
+
+            context.Skills.AddRange(existingSkills);
+            context.SaveChanges();
+
+            //Sanity Check
+            Assert.Equal(4, await context.Skills.CountAsync());
+
+            using (var logic = new SkillLogic(skillRepository, userRepositoryMock.Object, projectRepositoryMock.Object))
+            {
+                var results = await logic.SearchAsync("cook");
+
+                Assert.Equal(3, results.Count());
+                Assert.Equal(existingSkills[0].Name, results.ToArray()[0].Name);
+                Assert.Equal(existingSkills[1].Name, results.ToArray()[1].Name);
+                Assert.Equal(existingSkills[2].Name, results.ToArray()[2].Name);
+            }
+        }
+
+        [Fact]
+        public async void SearchAsync_GivenNoMatchingSkills_ReturnsEmptyEnumerable()
+        {
+            var existingSkills = new Skill[]
+            {
+                new Skill() { Id = 1, Name = "Cooking" },
+                new Skill() { Id = 2, Name = "Cooking Bacon"},
+                new Skill() { Id = 3, Name = "Burning things while trying to cook"},
+                new Skill() { Id = 4, Name = "Yoddeling"}
+            };
+
+            skillRepository = new SkillRepository(setupContextForIntegrationTests());
+
+            context.Skills.AddRange(existingSkills);
+            context.SaveChanges();
+
+            //Sanity Check
+            Assert.Equal(4, await context.Skills.CountAsync());
+
+            using (var logic = new SkillLogic(skillRepository, userRepositoryMock.Object, projectRepositoryMock.Object))
+            {
+                var result = await logic.FindExactAsync("cooking");
+
+                Assert.Equal(existingSkills[0].Name, result.Name);
+            }
+        }
+
+        private CrowdSparkContext setupContextForIntegrationTests()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            var builder = new DbContextOptionsBuilder<CrowdSparkContext>()
+                .UseSqlite(connection);
+
+            context = new CrowdSparkContext(builder.Options);
+            context.Database.EnsureCreated();
+            context.Database.BeginTransaction();
+
+            return context;
+        }
+
+        #endregion
+
     }
 }
