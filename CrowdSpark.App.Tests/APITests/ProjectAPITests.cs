c85ebd61f5859ffaa225dab4ceafbee9b979f2e7
Robert Erdoedy <robert.erdoedy@yahoo.com>
2017-12-17 17:01:27 +0100

    added ProjectAPI Test skeleton

---
+++ CrowdSpark.App.Tests/APITests/ProjectAPITests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using CrowdSpark.App.Models;
+
+namespace CrowdSpark.App.Tests.APITests
+{
+    [TestClass]
+    public class ProjectAPITests
+    {
+        Mock<IProjectAPI> projectAPI;
+        public ProjectAPITests()
+        {
+            projectAPI = new Mock<IProjectAPI>();
+        }
+
+        [TestMethod]
+        public void Test_returning_shit()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void Test_returning_crap()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
