ad97c008be94f65f327f04aef731cb925b2e7683
Robert Erdoedy <robert.erdoedy@yahoo.com>
2017-12-18 13:53:47 +0100

    implemented custom HttpHandler for UWP

---
+++ CrowdSpark.App/Models/IHttpHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrowdSpark.App
+{
+    public interface IHttpHandler : IDisposable
+    {
+        Task<HttpResponseMessage> GetAsync(string url);
+        Task<HttpResponseMessage> PostAsync(string url, HttpContent content);
+        Task<HttpResponseMessage> PutAsync(string url, HttpContent content);
+        Task<HttpResponseMessage> DeleteAsync(string url);
+        Uri BaseAddress { get; set; }
+        HttpRequestHeaders DefaultRequestHeaders { get; }
+
+    }
+}
